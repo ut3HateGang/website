60d585739cc07010565b5db6b654be052f34ebc1
Tristan PINTO <tristan.pin81@gmail.com>
2023-12-08 02:19:11 +0100

    Create Snake page

---
+++ src/app/components/navigation/snake/snake.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'snake',
+  templateUrl: './snake.component.html',
+  styleUrls: ['./snake.component.scss']
+})
+export class SnakeComponent {
+
+}
